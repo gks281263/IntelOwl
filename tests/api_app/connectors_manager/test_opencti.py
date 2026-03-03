15c013d0916d2695e314ed3d33a7da630b981d60
gks281263 <gks281263@gmail.com>
2026-03-03 13:33:49 +0530

    chore(opencti): fix ruff lint on tests

--- tests/api_app/connectors_manager/test_opencti.py
+++ tests/api_app/connectors_manager/test_opencti.py
@@ -1,5 +1,5 @@
 # This file is a part of IntelOwl https://github.com/intelowlproject/IntelOwl
-# See the file 'LICENSE' for copying permission. 
+# See the file 'LICENSE' for copying permission.
 
 import os
 from unittest import skipUnless
